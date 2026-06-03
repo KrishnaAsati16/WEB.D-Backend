588aa08bae9bbe26c812ef1ac2df4397cdb0d16a
Krishna Asati <krishnaasati2006@gmail.com>
2026-06-03 15:49:55 +0530

    Multer

---
+++ utils/cloudinary.js
@@ -0,0 +1,41 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+import {v2 as cloudinary} from 'cloudinary'
+
+cloudinary.config({
+   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret:process.env.CLOUDINARY_API_SECRET
+})
+
+
+const uploadOnClodinary = async (localFilePath) => {
+    try {
+          if(!localFilePath) return null
+          // upload  the file on cloudinary
+         const response = await cloudinary.uploader.upload
+          (localFilePath,{
+            resource_type : "auto"
+          })
+          // file has been uploaded successfull
+          console.log("file is uploaded successfully on cloudinary",response.url)
+          return response
+    }
+    catch(error) {
+        fs.unlinkSync(localFilePath)  // remove the locally saved temporary file as the uploade operation got failed
+        return null
+
+    }
+}
+
+
+cloudinary.v2.uploader
+.upload("dog.mp4", {
+  resource_type: "video", 
+  public_id: "my_dog",
+  overwrite: true, 
+  notification_url: "https://mysite.example.com/notify_endpoint"})
+.then(result=>console.log(result));
+
+export {uploadOnClodinary}
